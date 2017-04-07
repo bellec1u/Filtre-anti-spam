8cc4cebb6370f83ef88a846fedbb692320a4c5d2
bellec1u <leopold.bellec@gmail.com>
2017-04-07 16:07:57 +0200

    [ADD] stats mais pas sur que ca marche

--- src/classifier/Bayes.java
+++ src/classifier/Bayes.java
@@ -1,42 +1,65 @@
 package classifier;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Bayes {
 
 	private Dictionary dictionary;
-	private ArrayList<Message> message;
+	private HashMap<String, Integer> vectorHam;
+	private HashMap<String, Integer> vectorSpam;
 
 	private final static String BASE_APP_HAM = "./base/baseapp/ham/";
 	private final static String BASE_APP_SPAM = "./base/baseapp/spam/";
 
 	public Bayes(Dictionary d) {
 		this.dictionary = d;
-		this.message = new ArrayList<Message>();
+		
+		this.vectorHam = new HashMap<String, Integer>(d.getBase().size());
+		for (String word : dictionary.getBase())
+			vectorHam.put(word, 0);
+		
+		this.vectorSpam = new HashMap<String, Integer>(d.getBase().size());
+		for (String word : dictionary.getBase())
+			vectorSpam.put(word, 0);
 	}
 
-	public void analysisBaseApp(int mHam, int mSpam) {
-		// ham  ---------- ---------- ---------- ---------- ---------- ---------- ----------
-		System.out.println("Analyse des ham de baseapp ...");
-		String[] filesName = new File( this.BASE_APP_HAM ).list();
+	public void analysisBaseApp(int mHam, int mSpam) {		
+		// HAM  ---------- ---------- ---------- ---------- ---------- ---------- ----------
+		System.out.println("Apprentissage HAM ...");
+		ArrayList<Message> messages = new ArrayList<Message>();
+		String[] filesName = new File( Bayes.BASE_APP_HAM ).list();
 		int cmpt = 0;
 		while (cmpt < mHam || cmpt < filesName.length) {
-			this.message.add( new Message(this.BASE_APP_HAM + filesName[cmpt], this.dictionary) );
+			messages.add( new Message(Bayes.BASE_APP_HAM + filesName[cmpt], this.dictionary) );
 			cmpt++;
 		}
+		
+		// calcul des probas pour les HAM
+		for (Message m : messages) {
+			for (Map.Entry<String, Integer> entry : m.getVector().entrySet()) {
+				if (entry.getValue() != 0)
+					this.vectorHam.put(entry.getKey(), this.vectorHam.get(entry.getKey()) + 1);
+			}	
+		}
+		for (Map.Entry<String, Integer> entry : this.vectorHam.entrySet()) {
+System.out.println(entry.getKey() + " \t "+entry.getValue());
+		}
 
-		// spam ---------- ---------- ---------- ---------- ---------- ---------- ----------
-		System.out.println("Analyse des spam de baseapp ...");
-		filesName = new File( this.BASE_APP_SPAM ).list();
+		// SPAM ---------- ---------- ---------- ---------- ---------- ---------- ----------
+		System.out.println("Apprentissage SPAM ...");
+		messages = new ArrayList<Message>();
+		filesName = new File( Bayes.BASE_APP_SPAM ).list();
 		cmpt = 0;
 		while (cmpt < mHam || cmpt < filesName.length) {
-			this.message.add( new Message(this.BASE_APP_SPAM + filesName[cmpt], this.dictionary) );
+			messages.add( new Message(Bayes.BASE_APP_SPAM + filesName[cmpt], this.dictionary) );
 			cmpt++;
 		}
 
-		System.out.println("Fin d'analyse");
+		System.out.println("Fin d'apprentissage");
 	}
+
 
 }
