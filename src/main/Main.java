87c204bf4c296da75ab2ccc5f72364b46c57445e
bellec1u <leopold.bellec@gmail.com>
2017-04-07 14:40:56 +0200

    Merge

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,8 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		Dictionary d = new Dictionary("./base/dictionnaire1000en.txt");
+		String pathDictionnary = "./base/dictionnaire1000en.txt";
+		Dictionary d = new Dictionary(pathDictionnary);
 	}
 
 }
