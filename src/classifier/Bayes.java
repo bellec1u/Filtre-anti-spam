adc167cbf9b4ddaded90471086a442182faaed69
bellec1u <leopold.bellec@gmail.com>
2017-04-07 18:44:25 +0200

    [EDT] ajout calcul stats

--- src/classifier/Bayes.java
+++ src/classifier/Bayes.java
@@ -4,12 +4,13 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Bayes {
 
 	private Dictionary dictionary;
-	private HashMap<String, Integer> vectorHam;
-	private HashMap<String, Integer> vectorSpam;
+	private HashMap<String, Double> vectorHam;
+	private HashMap<String, Double> vectorSpam;
 
 	private final static String BASE_APP_HAM = "./base/baseapp/ham/";
 	private final static String BASE_APP_SPAM = "./base/baseapp/spam/";
@@ -17,47 +18,66 @@ public class Bayes {
 	public Bayes(Dictionary d) {
 		this.dictionary = d;
 		
-		this.vectorHam = new HashMap<String, Integer>(d.getBase().size());
+		this.vectorHam = new HashMap<String, Double>(d.getBase().size());
 		for (String word : dictionary.getBase())
-			vectorHam.put(word, 0);
+			vectorHam.put(word, 0.0);
 		
-		this.vectorSpam = new HashMap<String, Integer>(d.getBase().size());
+		this.vectorSpam = new HashMap<String, Double>(d.getBase().size());
 		for (String word : dictionary.getBase())
-			vectorSpam.put(word, 0);
+			vectorSpam.put(word, 0.0);
 	}
 
 	public void analysisBaseApp(int mHam, int mSpam) {		
 		// HAM  ---------- ---------- ---------- ---------- ---------- ---------- ----------
 		System.out.println("Apprentissage HAM ...");
-		ArrayList<Message> messages = new ArrayList<Message>();
 		String[] filesName = new File( Bayes.BASE_APP_HAM ).list();
 		int cmpt = 0;
 		while (cmpt < mHam || cmpt < filesName.length) {
-			messages.add( new Message(Bayes.BASE_APP_HAM + filesName[cmpt], this.dictionary) );
+			Message m = new Message(Bayes.BASE_APP_HAM + filesName[cmpt], this.dictionary);
+			for (Map.Entry<String, Integer> entry : m.getVector().entrySet())
+				if (entry.getValue() != 0)
+					// on compte combien de fois on retrouve le meme mot dans les HAM
+					this.vectorHam.put(entry.getKey(), this.vectorHam.get(entry.getKey()) + 1);	
 			cmpt++;
 		}
 		
-		// calcul des probas pour les HAM
-		for (Message m : messages) {
-			for (Map.Entry<String, Integer> entry : m.getVector().entrySet()) {
-				if (entry.getValue() != 0)
-					this.vectorHam.put(entry.getKey(), this.vectorHam.get(entry.getKey()) + 1);
-			}	
-		}
-		for (Map.Entry<String, Integer> entry : this.vectorHam.entrySet()) {
-			System.out.println(entry.getKey() + " \t "+entry.getValue());
-		}
-
+		// Calcul des stats des mots des HAM
+		for (Map.Entry<String, Double> entry : this.vectorHam.entrySet())
+			if (entry.getValue() != 0)
+				// on compte combien de fois on retrouve le meme mot dans les HAM
+				this.vectorHam.put(entry.getKey(), 
+						this.vectorHam.get(entry.getKey()) / mHam);
+		
 		// SPAM ---------- ---------- ---------- ---------- ---------- ---------- ----------
 		System.out.println("Apprentissage SPAM ...");
-		messages = new ArrayList<Message>();
 		filesName = new File( Bayes.BASE_APP_SPAM ).list();
 		cmpt = 0;
 		while (cmpt < mHam || cmpt < filesName.length) {
-			messages.add( new Message(Bayes.BASE_APP_SPAM + filesName[cmpt], this.dictionary) );
+			Message m = new Message(Bayes.BASE_APP_SPAM + filesName[cmpt], this.dictionary);
+			for (Entry<String, Integer> entry : m.getVector().entrySet())
+				if (entry.getValue() != 0)
+					// on compte combien de fois on retrouve le meme mot dans les SPAM
+					this.vectorSpam.put(entry.getKey(), this.vectorSpam.get(entry.getKey()) + 1);
 			cmpt++;
 		}
 
+		// Calcul des stats des mots des SPAM
+		for (Map.Entry<String, Double> entry : this.vectorSpam.entrySet())
+			if (entry.getValue() != 0)
+				// on compte combien de fois on retrouve le meme mot dans les HAM
+				this.vectorSpam.put(entry.getKey(), 
+						this.vectorSpam.get(entry.getKey()) / mSpam);
+		
+		
+		
+		
+		
+		// comparaison des valeures obtenues
+		for (Map.Entry entry : this.vectorHam.entrySet()) {
+			System.out.println(entry.getKey()+" \t "+entry.getValue()+" \t "+
+						this.vectorSpam.get(entry.getKey()));
+		}
+		
 		System.out.println("Fin d'apprentissage");
 	}
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,11 +6,9 @@ import classifier.Dictionary;
 public class Main {
 
 	public static void main(String[] args) {
-
 		Dictionary d = new Dictionary();
 		Bayes b = new Bayes(d);
 		b.analysisBaseApp(200, 200);
-		
 	}
 
 }
